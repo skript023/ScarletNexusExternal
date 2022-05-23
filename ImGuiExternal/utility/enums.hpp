6bfa1c63b92ef735e10fa8dd5e6cd49299474d8c
skript023 <fatur_muhammad@bk.ru>
2022-05-24 02:33:14 +0700

    First Commit

---
+++ ImGuiExternal/utility/enums.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+enum player_skill : std::int32_t
+{
+    pyrokinesis = 0,
+    sclerokinesis = 1,
+    clairvoyance = 2,
+    teleportation = 3,
+    invisibility = 4,
+    electrokinesis = 5,
+    duplication = 6,
+    hypervelocity = 7,
+    psychokinesis = 8,
+    psychokinesis_kasane = 9,
+    no_skill = 10
+};
