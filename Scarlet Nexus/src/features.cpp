f49248a52785a73bd91bbe779ec41e42b5e884de
skript023 <fatur_muhammad@bk.ru>
2022-05-24 21:08:32 +0700

    Fixing Content

---
+++ Scarlet Nexus/src/features.cpp
@@ -0,0 +1,25 @@
+#include "features.hpp"
+#include "utility/utility.hpp"
+#include "utility/menu_settings.hpp"
+
+namespace ellohim::features
+{
+	void run_per_tick()
+	{
+        if (option.infinite_psychic)
+        {
+            utility::set_player_pychic(1000.f);
+        }
+
+        utility::remove_sas_cooldown(option.no_sas_cooldown);
+        utility::set_infinite_sas_duration(option.infinite_sas_duration);
+
+        utility::set_infinite_brain_dive(option.infinite_brain_dive);
+        utility::instant_brain_field(option.instant_brain_field);
+        utility::enter_brain_dive(option.enter_brain_dive);
+
+        utility::infinite_battle_points(option.infinite_battle_point);
+        utility::item_usage_no_cooldown(option.no_items_cooldown);
+        utility::infinite_player_credits(option.infinite_credits);
+	}
+}
---
+++ Scarlet Nexus/src/utility/menu_settings.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+class menu_settings
+{
+public:
+	static inline bool infinite_psychic{ false };
+	static inline bool no_sas_cooldown{ false };
+	static inline bool infinite_sas_duration{ false };
+	static inline bool infinite_brain_dive{ false };
+	static inline bool instant_brain_field{ false };
+	static inline bool infinite_battle_point{ false };
+	static inline bool no_items_cooldown{ false };
+	static inline bool enter_brain_dive{ false };
+	static inline bool infinite_credits{ false };
+
+	
+
+
+};
+
+inline menu_settings option{};
